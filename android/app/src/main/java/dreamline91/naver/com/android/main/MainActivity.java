2f52a98f1049c58fd2c29b463ed1bc8f9a0e2027
jaejunha <dreamline91@naver.com>
2017-11-15 10:30:16 +0900

    make android project

---
+++ android/app/src/main/java/dreamline91/naver/com/android/main/MainActivity.java
@@ -0,0 +1,19 @@
+package dreamline91.naver.com.android.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import dreamline91.naver.com.android.R;
+
+/**
+ * Created by dream on 2017-11-15.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
