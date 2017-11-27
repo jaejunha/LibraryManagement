58389eba504773a74234241cd25671596dfe15aa
Yeon Gyo Kim <dusry97@ajou.ac.kr>
2017-11-27 18:51:14 +0900

    LCD 추가

---
+++ android/app/build/generated/source/buildConfig/androidTest/debug/dreamline91/naver/com/android/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dreamline91.naver.com.android.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "dreamline91.naver.com.android.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
