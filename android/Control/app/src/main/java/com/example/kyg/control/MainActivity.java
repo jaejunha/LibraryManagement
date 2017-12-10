fc64c8cdde8567b61e7bdc0cd32b2b545b3eef88
Yeon Gyo Kim <dusry97@ajou.ac.kr>
2017-12-10 15:59:23 +0900

    control

---
+++ android/Control/app/src/main/java/com/example/kyg/control/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.kyg.control;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int window;
+    private int bright;
+    public TextView Win;
+    public TextView Br;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Win=(TextView)findViewById(R.id.win);
+        Br=(TextView)findViewById(R.id.br);
+        SeekBar WinSeekBar=(SeekBar)findViewById(R.id.winSeek);
+        SeekBar BrightSeekBar=(SeekBar)findViewById(R.id.brSeek);
+        //아두이노에서 현재 창문 열린정도, 밝기를 받아와서 window,bright에 initiate
+        WinSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                window=seekBar.getProgress();
+                Win.setText("창문 열기 : "+window+"%");
+                //아두이노에 창문 값 전송
+            }
+        });
+        BrightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                bright=seekBar.getProgress();
+                Br.setText("밝기 조절 : "+bright+"%");
+                //아두이노에 밝기 값 전송
+            }
+        });
+    }
+}
